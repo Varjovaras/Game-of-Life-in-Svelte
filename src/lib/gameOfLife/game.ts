79fe4370335f6e656dd7fd6e536b78b12fae42a9
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-03-17 02:30:19 +0200

    small refactor into util.ts

--- src/lib/gameOfLife/game.ts
+++ src/lib/gameOfLife/game.ts
@@ -1,41 +1,5 @@
-import type { Grid, Row, Game, Cell } from './types';
-import { randomizer } from './utils';
-
-const newCell = (alive = false): Cell => {
-	return alive;
-};
-
-const newRules = () => {
-	return {
-		underpopulation: 1,
-		overpopulation: 4,
-		reproduction: 3
-	};
-};
-
-const newGrid = (xSize: number, ySize: number): Grid => {
-	const grid: Grid = [];
-	for (let i = 0; i < ySize; i++) {
-		const row: Row = [];
-		for (let j = 0; j < xSize; j++) {
-			row.push(newCell(randomizer()));
-		}
-		grid.push(row);
-	}
-	return grid;
-};
-
-const newEmptyGrid = (xSize: number, ySize: number): Grid => {
-	const grid: Grid = [];
-	for (let i = 0; i < ySize; i++) {
-		const row: Row = [];
-		for (let j = 0; j < xSize; j++) {
-			row.push(newCell());
-		}
-		grid.push(row);
-	}
-	return grid;
-};
+import type { Game, Cell } from './types';
+import { getAdjacentCells, newEmptyGrid, newGrid, newRules } from './utils';
 
 export const newGame = (x: number, y: number): Game => {
 	const game: Game = { rules: newRules(), grid: newGrid(x, y), generation: 0, gameOver: false };
@@ -83,27 +47,4 @@ export const nextGeneration = (game: Game): Game => {
 	newGame.grid = newGrid;
 	newGame.generation += 1;
 	return newGame;
-};
-
-const getAdjacentCells = (grid: Grid, x: number, y: number): Cell[] => {
-	const adjacentCells: Cell[] = [];
-
-	for (let i = -1; i <= 1; i++) {
-		for (let j = -1; j <= 1; j++) {
-			const newX = x + i;
-			const newY = y + j;
-
-			// Skip the cell itself
-			if (i === 0 && j === 0) {
-				continue;
-			}
-
-			// Check if the coordinates are within the grid
-			if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length) {
-				adjacentCells.push(grid[newX][newY]);
-			}
-		}
-	}
-
-	return adjacentCells;
 };
--- src/lib/gameOfLife/utils.ts
+++ src/lib/gameOfLife/utils.ts
@@ -1,3 +1,64 @@
+import type { Cell, Grid, Row } from './types';
+
 export const randomizer = () => {
 	return Math.random() > 0.5;
+};
+
+export const newCell = (alive = false): Cell => {
+	return alive;
+};
+
+export const newRules = () => {
+	return {
+		underpopulation: 1,
+		overpopulation: 4,
+		reproduction: 3
+	};
+};
+
+export const newGrid = (xSize: number, ySize: number): Grid => {
+	const grid: Grid = [];
+	for (let i = 0; i < ySize; i++) {
+		const row: Row = [];
+		for (let j = 0; j < xSize; j++) {
+			row.push(newCell(randomizer()));
+		}
+		grid.push(row);
+	}
+	return grid;
+};
+
+export const newEmptyGrid = (xSize: number, ySize: number): Grid => {
+	const grid: Grid = [];
+	for (let i = 0; i < ySize; i++) {
+		const row: Row = [];
+		for (let j = 0; j < xSize; j++) {
+			row.push(newCell());
+		}
+		grid.push(row);
+	}
+	return grid;
+};
+
+export const getAdjacentCells = (grid: Grid, x: number, y: number): Cell[] => {
+	const adjacentCells: Cell[] = [];
+
+	for (let i = -1; i <= 1; i++) {
+		for (let j = -1; j <= 1; j++) {
+			const newX = x + i;
+			const newY = y + j;
+
+			// Skip the cell itself
+			if (i === 0 && j === 0) {
+				continue;
+			}
+
+			// Check if the coordinates are within the grid
+			if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length) {
+				adjacentCells.push(grid[newX][newY]);
+			}
+		}
+	}
+
+	return adjacentCells;
 };
