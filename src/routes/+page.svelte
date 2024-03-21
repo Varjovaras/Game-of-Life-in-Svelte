dcf94ef84181d8e30fbdde7590c7a3b4c82995ca
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-03-21 12:28:46 +0200

    center range sliders and remove empty space around cells

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,22 @@
 	import { newEmptyGame, newGame, nextGeneration } from '$lib/gameOfLife/game';
 	import { RangeSlider } from '@skeletonlabs/skeleton';
 
-	// biome-ignore lint/style/useConst: <explanation>
-	let xAxis = 20;
-	// biome-ignore lint/style/useConst: <explanation>
-	let yAxis = 20;
-	// biome-ignore lint/style/useConst: <explanation>
-	let max = 60;
+	const gridDetails = {
+		x: 20,
+		y: 10,
+		max: 60
+	};
+
 	let isPlaying = false;
 	// biome-ignore lint/suspicious/noConfusingLabels: svelte tag
-	$: game = newEmptyGame(xAxis, yAxis);
+	$: game = newEmptyGame(gridDetails.x, gridDetails.y);
 
 	const randomizeGrid = () => {
-		game = newGame(xAxis, yAxis);
+		game = newGame(gridDetails.x, gridDetails.y);
 	};
 
 	const resetGrid = () => {
-		game = newEmptyGame(xAxis, yAxis);
+		game = newEmptyGame(gridDetails.x, gridDetails.y);
 		isPlaying = false;
 	};
 
@@ -45,43 +45,47 @@
 
 		<div class="space-x-0 space-y-0">
 			{#each game.grid as row, x}
-				<div class="flex justify-center text-center">
+				<div class="flex justify-center">
 					{#each row as cell, y}
-						<button class="" on:click={(cell) => toggleCell(x, y)}>
+						<button
+							style="padding:0; margin:0; border:none; box-sizing: border-box; line-height: 1;"
+							on:click={() => toggleCell(x, y)}
+						>
 							{cell ? '🟩' : '⬛'}
 						</button>
 					{/each}
 				</div>
 			{/each}
+
 			<p>
 				generation {game.generation}
 			</p>
-			<RangeSlider
-				name="range-slider"
-				bind:value={xAxis}
-				on:change={randomizeGrid}
-				max={60}
-				step={1}
-				ticked
-			>
-				<div class="flex items-center justify-between">
-					<div class="font-bold">x axis</div>
-					<div class="text-xs">{xAxis} / {max}</div>
-				</div>
-			</RangeSlider>
-			<RangeSlider
-				name="range-slider"
-				bind:value={yAxis}
-				on:change={randomizeGrid}
-				max={60}
-				step={1}
-				ticked
-			>
-				<div class="flex items-center justify-between">
-					<div class="font-bold">y axis</div>
-					<div class="text-xs">{yAxis} / {max}</div>
-				</div>
-			</RangeSlider>
+			<div class="justify-center px-12">
+				<RangeSlider
+					name="range-slider"
+					bind:value={gridDetails.x}
+					on:change={randomizeGrid}
+					max={gridDetails.max}
+					step={1}
+				>
+					<div class="flex items-center justify-between">
+						<div class="font-bold">x axis</div>
+						<div class="text-xs">{gridDetails.x} / {gridDetails.max}</div>
+					</div>
+				</RangeSlider>
+				<RangeSlider
+					name="range-slider"
+					bind:value={gridDetails.y}
+					on:change={randomizeGrid}
+					max={gridDetails.max}
+					step={1}
+				>
+					<div class="flex items-center justify-between">
+						<div class="font-bold">y axis</div>
+						<div class="text-xs">{gridDetails.y} / {gridDetails.max}</div>
+					</div>
+				</RangeSlider>
+			</div>
 		</div>
 	</div>
 </div>
