79eac6050093bbd0431918007d7145c6bb9b008a
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-03-21 09:35:48 +0200

    range sliders to resize grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
 	import { newEmptyGame, newGame, nextGeneration } from '$lib/gameOfLife/game';
+	import { RangeSlider } from '@skeletonlabs/skeleton';
 
-	const dimensions = {
-		x: 20,
-		y: 20
-	};
-
-	let game = newEmptyGame(dimensions.x, dimensions.y);
+	// biome-ignore lint/style/useConst: <explanation>
+	let xAxis = 20;
+	// biome-ignore lint/style/useConst: <explanation>
+	let yAxis = 20;
+	// biome-ignore lint/style/useConst: <explanation>
+	let max = 60;
 	let isPlaying = false;
+	// biome-ignore lint/suspicious/noConfusingLabels: svelte tag
+	$: game = newEmptyGame(xAxis, yAxis);
 
 	const randomizeGrid = () => {
-		game = newGame(dimensions.x, dimensions.y);
+		game = newGame(xAxis, yAxis);
 	};
 
 	const resetGrid = () => {
-		game = newEmptyGame(dimensions.x, dimensions.y);
+		game = newEmptyGame(xAxis, yAxis);
 		isPlaying = false;
 	};
 
 	const play = () => {
 		isPlaying = true;
-		game = nextGeneration(game);
 		game = nextGeneration(game);
 		setTimeout(() => {
 			if (isPlaying) {
@@ -39,7 +41,6 @@
 		<h1 class="h1">Game of <span class="text-green-400">life</span> Simulator</h1>
 		<button class="variant-filled btn" on:click={randomizeGrid}>random board</button>
 		<button class="variant-filled btn" on:click={resetGrid}>reset grid</button>
-
 		<button class="variant-filled btn" on:click={play}>play game</button>
 
 		<div class="space-x-0 space-y-0">
@@ -52,6 +53,35 @@
 					{/each}
 				</div>
 			{/each}
+			<p>
+				generation {game.generation}
+			</p>
+			<RangeSlider
+				name="range-slider"
+				bind:value={xAxis}
+				on:change={randomizeGrid}
+				max={60}
+				step={1}
+				ticked
+			>
+				<div class="flex items-center justify-between">
+					<div class="font-bold">x axis</div>
+					<div class="text-xs">{xAxis} / {max}</div>
+				</div>
+			</RangeSlider>
+			<RangeSlider
+				name="range-slider"
+				bind:value={yAxis}
+				on:change={randomizeGrid}
+				max={60}
+				step={1}
+				ticked
+			>
+				<div class="flex items-center justify-between">
+					<div class="font-bold">y axis</div>
+					<div class="text-xs">{yAxis} / {max}</div>
+				</div>
+			</RangeSlider>
 		</div>
 	</div>
 </div>
